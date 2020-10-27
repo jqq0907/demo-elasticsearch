1520b0350b8ecad88a44ce371dc6e5cdcddfa2ca
jqq0907 <56289398+jqq0907@users.noreply.github.com>
2020-10-27 23:41:39 +0800

    latest

--- README.md
+++ README.md
@@ -1,3 +1,567 @@
 #elasticsearch
 
-##
+##form和size的ES查询原理
+    1.将用户指定的关键词进行分词
+    2.去分词库中检索关键词，得到多个id
+    3.根据id去分片中拉去指定的数据
+    4.将数据根据score(匹配度)进行排序
+    5.根据from的值将查询结果舍弃一部分
+    6.返回结果  
+##scroll查询原理
+    1.将用户指定的关键词进行分词
+    2.去分词库中检索关键词，得到多个id
+    3.将文档id存放在es上下文中
+    4.根据指定的size大小去es中检索指定个数的数据，拿完数据的文档id，会从上下文中移除
+    5.如果需要下一页数据，直接去es的上下文中查找
+    
+##es的Restful语法
+
+###1.创建一个索引
+```
+PUT /index1 #索引名
+{
+"settings": {
+   "number_of_shards": 1, #分片数
+   "number_of_replicas": 1 #备份数
+}
+}
+```
+###2.查看索引映射关系
+```
+GET /book/_mapping #book为索引名
+```
+###3.删除索引
+```
+DELETE index1
+```
+###4.创建索引并指定结构
+```
+PUT /book
+{
+  "settings": {
+    "number_of_replicas": 1,
+    " ": 1
+  },
+  "mappings": {
+    "properties": {
+      "name": {
+        "type": "text",
+        "analyzer": "ik_max_word",
+        "index": true,
+        "store": false
+      },
+      "author": {
+        "type": "keyword"
+      },
+      "count": {
+        "type": "long"
+      },
+      "onSale": {
+        "type": "date",
+        "format": "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
+      },
+      "desc": {
+        "type": "text",
+        "analyzer": "ik_max_word"
+      }
+    }
+  }
+}
+```
+###5.添加文档随机id
+```
+POST /book/_doc
+{
+  "name": "西游记",
+  "author": "吴道子",
+  "count": 12,
+  "onSale": "2020-10-25",
+  "desc": "西游记测试"
+}
+```
+###6.添加文档指定id或者根据id修改文档
+```
+POST /book/_doc/1 #1是id
+{
+  "name": "java基础",
+  "author": "刘安",
+  "count": 100,
+  "onSale": "2020-10-25",
+  "desc": "java"
+}
+```
+###7.根据id删除
+```
+DELETE /book/_doc/1
+```
+###8.查询所有match_all
+```
+GET /book/_search
+{
+  "query": {
+    "match_all": {
+      
+    }
+  },
+    "_source": {
+    "includes": ["name","author"], #包括的field
+    "excludes": "onSale"
+  }
+}
+```
+###9.match根据field筛选查询
+```
+GET /book/_search
+{
+  "query": {
+    "match": {
+      "name": "西游记" #name字段名
+    }
+  }
+}
+```
+###10.match布尔查询
+```
+POST /book/_search
+{
+  "query": {
+    "match": {
+      "name": {
+        "query": "西游 心里",
+        "operator": "or" #or and
+      }
+    }
+  }
+}
+```
+###11.multi_match多个field对应一个text
+```
+POST /book/_search
+{
+  "query": {
+    "multi_match": {
+      "query": "西游",
+      "fields": ["name","desc"] #多个field
+    }
+  }
+}
+```
+###12.term查询，代表完全匹配，搜索前不会对搜索的关键词进行分词，直接匹配
+```
+POST /book/_search
+{
+  "query": {
+    "term": {
+      "name": {
+        "value": "西游"
+      }
+    }
+  }
+}
+```
+###13.trems查询，搜索前不会对搜索的关键词进行分词，针对一个field对应多个text
+```
+POST /book/_search
+{
+  "query": {
+    "terms": {
+      "name": [
+        "西游",
+        "西游记"
+      ]
+    }
+  }
+}
+```
+###14.根据id查询
+```
+GET /book/_doc/1 #book索引名，1为id
+```
+##15.根据ids查询
+```
+POST /book/_search
+{
+  "query": {
+    "ids": {
+      "values": ["1","3"]
+    }
+  }
+}
+```
+###16.prefix前缀匹配查询
+```
+POST /book/_search
+{
+  "query": {
+    "prefix": {
+      "name": {
+        "value": "心"
+      }
+    }
+  }
+}
+```
+###17.fuzzy模糊查询
+```
+POST /book/_search
+{
+  "query": {
+    "fuzzy": {
+      "name": {
+        "value": "java", #允许有错字
+        "prefix_length": 4 #指定前面四个字必须匹配
+      }
+    }
+  }
+}
+```
+###18.wildcard通配符查询
+```
+POST /book/_search
+{
+  "query": {
+    "wildcard": {
+      "name": {
+        "value": "java*"  #*代表所有,?代表一个
+      }
+    }
+  }
+}
+```
+###19.regexp自定义正则匹配查询
+```
+POST /book/_search
+{
+  "query": {
+    "regexp": {
+      "name": "[a-z]*" #正则表达式
+    }
+  }
+}
+```
+###20.rang范围查询
+```
+POST /book/_search
+{
+  "query": {
+    "range": {
+      "count": {
+        "gte": 10, #gte大于等于,gt大于
+        "lte": 150
+      }
+    }
+  }
+}
+```
+###21.scroll分页查询
+```
+#获取第一页数据
+POST /book/_search/?scroll=1m #scroll上下文存活时间1分钟
+{
+  "query": {
+    "match_all": {
+    
+    }
+  },
+  "size": 2, #每页大小
+  "sort": [ #排序
+    {
+      "count": {
+        "order": "desc"
+      }
+    }
+  ]
+}
+#下一页数据
+POST /_search/scroll
+{
+  "scroll_id": "FGluY2x1ZGVfY29udGV4dF91dWlkDXF1ZXJ5QW5kRmV0Y2gBFEhyREdaWFVCRTdhRmpFWXo4dFBSAAAAAAAAEEwWOGRPUUJna05UVnk2Q1dwWXI1VGJWQQ==", #scroll_id
+  "scroll": "1m"
+}
+#删除scroll上下文
+DELETE/_search/scroll/FGluY2x1ZGVfY29udGV4dF91dWlkDXF1ZXJ5QW5kRmV0Y2gBFEk3REhaWFVCRTdhRmpFWXpfOU44AAAAAAAAEFEWOGRPUUJna05UVnk2Q1dwWXI1VGJWQQ==
+```
+###22.delete_by_query,删除term,match查询出来的数据
+```
+POST /book/_delete_by_query
+{
+  "query": {
+    "range": {
+        "count": {
+          "lt": 1
+       }
+    }
+  }
+}
+```
+###23.复合bool查询
+```
+POST /book/_search
+{
+  "query": {
+    "bool": {
+      "should": [  #should是or
+        {
+          "term": {
+            "name": {
+              "value": "西游记"
+            }
+          }
+        }
+      ],
+      "must": [ #must代表and
+        {
+          "match": {
+            "desc": "java"
+          }
+        }
+      ],
+      "must_not": [ #must_not代表not
+        {
+          "match": {
+            "name": "心里"
+          }
+        }
+      ]
+    }
+  }
+}
+```
+###24.boosting查询，操作查询后的_score
+```
+POST /book/_search
+{
+  "query": {
+    "boosting": {
+      "positive": {
+        "match": {
+          "name": "java"
+        }
+      },
+      "negative": { 
+        "match": {
+          "desc": "java"
+        }
+      },
+      "negative_boost": 0.2 #查出来的数据满足positive后满足negative,就把数据的_score乘以negative_boost的值
+    }
+  }
+}
+```
+###25.filter查询，基于bool，不计算score，不会排序，会做缓存
+```
+POST /book/_search
+{
+  "query": {
+    "bool": {
+      "filter": [
+        {
+          "term": {
+            "name": "西游记"
+          }
+        }
+      ]
+    }
+  }
+}
+```
+###26.highlight高亮查询
+```
+POST /book/_search
+{
+  "query": {
+    "match": {
+      "name": "java"
+    }
+  },
+  "highlight": {
+    "fields": {
+      "name": {} #高亮field，多个
+    },
+    "pre_tags": "<font color='red'>", 
+    "post_tags": "</font>",
+    "fragment_size": 3 #  一段 fragment 包含多少个字符    
+  }
+}
+```
+###27. cardinality去重计数查询
+```
+POST /book/_search
+{
+  "aggs": {
+    "agg": { #名称
+      "cardinality": { #去重计数
+        "field": "author"
+      }
+    }
+  }
+}
+```
+###28.range范围统计，统计一定范围内出现的个数
+```
+#数值范围
+POST /book/_search
+{
+  "aggs": {
+    "agg": {
+      "range": {
+        "field": "count", #字段
+        "ranges": [
+          {
+            "from": 0,  #大于等于50
+            "to": 100 #小于100
+          },
+          {
+            "from": 100,
+            "to": 150
+          }
+        ]
+      }
+    }
+  }
+}
+#时间范围
+POST /book/_search
+{
+  "aggs": {
+    "agg": {
+      "date_range": {
+        "field": "onSale",
+        "format": "yyyy", #时间格式
+        "ranges": [
+          {
+            "from": "2020",
+            "to": "2020"
+          }
+        ]
+      }
+    }
+  }
+}
+#ip范围
+POST /book/_search
+{
+  "aggs": {
+    "agg": {
+      "ip_range": {
+        "field": "ip",
+        "ranges": [
+          {
+            "from": "10.0.0.5",
+            "to": "10.0.0.10"
+          }
+        ]
+      }
+    }
+  }
+}
+```
+###29.extended_stats统计聚合查询，查询指定field的最大值、最小值、平均值...
+```
+POST /book/_search
+{
+  "aggs": {
+    "agg": {
+      "extended_stats": {
+        "field": "count"
+      }
+    }
+  }
+}
+```
+###30.地图
+```
+#添加地图所以
+PUT /map
+{
+  "mappings": {
+    "properties": {
+      "name": {
+        "type": "text"
+      },
+      "location": {
+        "type": "geo_point"
+      }
+    }
+  }
+}
+#添加数据
+POST /map/_doc
+{
+  "name": "天安门",
+  "location": {
+    "lon": 116.302509,
+    "lat": 39.991152
+  }
+}
+```
+###31.ES地图检索
+```
+geo_distance: 直线距离检索方式
+geo_bounding_box: 以两个点确定一个矩形，获取矩形内数据
+deo_polygon: 以多个点确认一个多边形
+
+POST /map/_search
+{
+  "query": {
+    "bool": {
+      "filter": {
+        "geo_distance": {
+          "distance": 4000, #半径
+          "distance_type": "arc", #圆形
+           "location": {
+              "lon": 116.433733,
+              "lat": 39.908404
+            }
+        }
+      }
+    }
+  }
+}
+
+POST /map/_search
+{
+  "query": {
+    "bool": {
+      "filter": {
+        "geo_bounding_box": {
+          "location": {
+            "top_left": { #左上角
+              "lat": 40.73,
+              "lon": -74.1
+            },
+            "bottom_right": { #右下角
+              "lat": 40.717,
+              "lon": -73.99
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+POST /map/_search
+{
+  "query": {
+    "bool": {
+      "filter": {
+        "geo_polygon": {
+          "location": {
+            "points": [ #多个点
+              {
+                "lat": 40.73,
+                "lon": -74.1
+              },
+              {
+                "lat": 40.83,
+                "lon": -75.1
+              }
+            ]
+          }
+        }
+      }
+    }
+  }
+}
+```
--- src/test/java/com/example/elasticsearch/DemoElasticsearchApplicationTests.java
+++ src/test/java/com/example/elasticsearch/DemoElasticsearchApplicationTests.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.example.pojo.Book;
 import com.example.pojo.Item;
 import net.minidev.json.JSONArray;
+import org.apache.lucene.util.ArrayUtil;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexAction;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.bulk.BulkRequest;
@@ -14,8 +15,7 @@ import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.*;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
@@ -25,28 +25,40 @@ import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.client.indices.GetIndexResponse;
+import org.elasticsearch.common.geo.GeoPoint;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.util.ArrayUtils;
 import org.elasticsearch.common.xcontent.XContent;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.Operator;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.BulkByScrollResponse;
+import org.elasticsearch.index.reindex.DeleteByQueryRequest;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.metrics.Cardinality;
+import org.elasticsearch.search.aggregations.metrics.ExtendedStats;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 import org.elasticsearch.search.fetch.subphase.highlight.AbstractHighlighterBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.elasticsearch.search.fetch.subphase.highlight.Highlighter;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.util.CollectionUtils;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -166,10 +178,10 @@ class DemoElasticsearchApplicationTests {
     @Test
     void addDocument() throws IOException {
         //1.数据
-        Book book = new Book("心理科", "夏至", 112L, new Date(), "交通出版社");
+        Book book = new Book("java从入门到精通", "李四", 212L, new Date(), "带你进入java的世界");
         //2.创建请求
         IndexRequest request = new IndexRequest("book");
-        request.id("1");
+        request.id("3");
         request.timeout(TimeValue.timeValueSeconds(1));
         request.timeout("1s"); //二选一
 
@@ -370,6 +382,7 @@ class DemoElasticsearchApplicationTests {
 
     /**
      * multi_match查询，多个field对应一个text
+     *
      * @throws IOException
      */
     @Test
@@ -378,11 +391,360 @@ class DemoElasticsearchApplicationTests {
         SearchRequest request = new SearchRequest("book");
         //2.创建查询条件
         SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-        sourceBuilder.query(QueryBuilders.multiMatchQuery("西游","name","desc"));
+        sourceBuilder.query(QueryBuilders.multiMatchQuery("西游", "name", "desc"));
         request.source(sourceBuilder);
         //3.执行查询
         SearchResponse response = client.search(request, RequestOptions.DEFAULT);
         //4.输出结果
         System.out.println(response);
+    }
+
+    /**
+     * 根据ids查询
+     *
+     * @throws IOException
+     */
+    @Test
+    void searchByIds() throws IOException {
+        //1.创建request
+        SearchRequest request = new SearchRequest("book");
+        //2.创建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.idsQuery().addIds("1", "3"));
+        request.source(sourceBuilder);
+        //3.执行查询
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //4.输出结果
+        System.out.println(response);
+    }
+
+    /**
+     * 根据id查询，类似查询索引
+     *
+     * @throws IOException
+     */
+    @Test
+    void searchById() throws IOException {
+        //1.创建request
+        GetRequest request = new GetRequest("book", "1");
+        //2.执行查询
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        //3.输出结果
+        System.out.println(response);
+    }
+
+    /**
+     * prefix前缀匹配查询
+     */
+    @Test
+    void searchPrefix() throws IOException {
+        //1.创建request
+        SearchRequest request = new SearchRequest("book");
+        //2.创建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.prefixQuery("desc", "java"));
+        request.source(sourceBuilder);
+        //3.执行查询
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //4.输出结果
+        System.out.println(response);
+    }
+
+    /**
+     * fuzzy模糊查询
+     */
+    @Test
+    void searchFuzzy() throws IOException {
+        //1.创建request
+        SearchRequest request = new SearchRequest("book");
+        //2.创建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.fuzzyQuery("desc", "javq")
+                .prefixLength(3)); //前面3个字不允许出错
+        request.source(sourceBuilder);
+        //3.执行查询
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //4.输出结果
+        System.out.println(response);
+    }
+
+    /**
+     * wildcard通配符查询
+     */
+    @Test
+    void searchWildcard() throws IOException {
+        //1.创建request
+        SearchRequest request = new SearchRequest("book");
+        //2.创建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.wildcardQuery("desc", "java*")); //*代表所有, ?代表一个
+        request.source(sourceBuilder);
+        //3.执行查询
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //4.输出结果
+        System.out.println(response);
+    }
+
+    /**
+     * regexp正则匹配查询
+     */
+    @Test
+    void searchRegexp() throws IOException {
+        //1.创建request
+        SearchRequest request = new SearchRequest("book");
+        //2.创建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.regexpQuery("desc", "[a-z]*")); //正则
+        request.source(sourceBuilder);
+        //3.执行查询
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //4.输出结果
+        System.out.println(response);
+    }
+
+    /**
+     * range范围查询
+     */
+    @Test
+    void searchRange() throws IOException {
+        //1.创建request
+        SearchRequest request = new SearchRequest("book");
+        //2.创建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.rangeQuery("count")
+                .gt(10)
+                .lt(150)); //gt、gte、lt、lte
+        request.source(sourceBuilder);
+        //3.执行查询
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //4.输出结果
+        System.out.println(response);
+    }
+
+    /**
+     * scroll分页查询
+     */
+    @Test
+    void searchScroll() throws IOException {
+        //1.创建request
+        SearchRequest request = new SearchRequest();
+        //2.指定scroll
+        request.scroll(TimeValue.timeValueMillis(1L)); //scroll_id存活时间
+        //3.创建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.size(1); //每页大小
+        sourceBuilder.sort("count", SortOrder.DESC); //排序
+        sourceBuilder.query(QueryBuilders.matchAllQuery());
+        //4.查询第一页数据，获取scroll_id
+        SearchResponse searchResponse = client.search(request, RequestOptions.DEFAULT);
+        String scrollId = searchResponse.getScrollId();
+        System.out.println(scrollId);
+        for (SearchHit hit : searchResponse.getHits().getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
+        System.out.println("------------------------");
+        //5.循环下一页
+        while (true) {
+            SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollId);
+            scrollRequest.scroll(TimeValue.timeValueMinutes(1L));
+            SearchResponse response = client.scroll(scrollRequest, RequestOptions.DEFAULT);
+            SearchHit[] hits = response.getHits().getHits();
+            if (hits.length > 0 && hits != null) {
+                for (SearchHit hit : hits) {
+                    System.out.println(hit.getSourceAsMap());
+                }
+            } else {
+                break;
+            }
+        }
+        System.out.println("------------------------");
+        //6.清除scroll_id
+        ClearScrollRequest clearScrollRequest = new ClearScrollRequest();
+        clearScrollRequest.addScrollId(scrollId);
+        ClearScrollResponse clearScrollResponse = client.clearScroll(clearScrollRequest, RequestOptions.DEFAULT);
+        System.out.println(clearScrollRequest);
+    }
+
+    /**
+     * delete_by_query 删除查询的数据
+     *
+     * @throws IOException
+     */
+    @Test
+    void delete_by_query() throws IOException {
+        //1.创建deleteByQueryRequest
+        DeleteByQueryRequest deleteByQueryRequest = new DeleteByQueryRequest("book");
+        //2.设置查询条件
+        deleteByQueryRequest.setQuery(QueryBuilders.rangeQuery("count").gt(10));
+        //3.执行
+        BulkByScrollResponse bulkByScrollResponse = client.deleteByQuery(deleteByQueryRequest, RequestOptions.DEFAULT);
+        //4.输出结果
+        System.out.println(bulkByScrollResponse.toString());
+    }
+
+    /**
+     * 复合bool查询
+     */
+    void searchBool() throws IOException {
+        //1.创建request
+        SearchRequest request = new SearchRequest();
+        //2.创建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.boolQuery()
+                .must(QueryBuilders.termQuery("name", "西游记")) //and
+                .mustNot(QueryBuilders.termQuery("name", "心里")) //not
+                .should(QueryBuilders.termQuery("desc", "西游记"))); //or
+
+        request.source(sourceBuilder);
+        //3.执行查询
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //4.输出结果
+        System.out.println(response);
+    }
+
+    /**
+     * boosting查询，操作查询后的_score,影响排序
+     */
+    @Test
+    void searchBoosting() throws IOException {
+        //1.创建request
+        SearchRequest request = new SearchRequest();
+        //2.创建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.boostingQuery(
+                QueryBuilders.termQuery("name", "java"),//满足positive后
+                QueryBuilders.rangeQuery("count").gt(50)) //再满足negative
+                .negativeBoost(0.1f)); //数据的_score乘以negative_boost的值
+
+        request.source(sourceBuilder);
+        //3.执行查询
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //4.输出结果
+        System.out.println(response);
+    }
+
+    /**
+     * .filter查询，基于bool，不计算score，不会排序，会做缓存
+     */
+    @Test
+    void searchFilter() throws IOException {
+        //1.创建request
+        SearchRequest request = new SearchRequest();
+        //2.创建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders
+                .boolQuery()
+                .filter(QueryBuilders.termQuery("name", "西游记")));
+
+        request.source(sourceBuilder);
+        //3.执行查询
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //4.输出结果
+        System.out.println(response);
+    }
+
+    /**
+     * highlight高亮查询
+     */
+    @Test
+    void searchHighLight() throws IOException {
+        //1.创建request
+        SearchRequest request = new SearchRequest();
+        //2.创建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.termsQuery("name", "西游记"));
+        //3.高亮条件
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder
+                .field("name", 10) //高亮字段，可以多个
+                .preTags("<font color='red'>") //高亮样式
+                .postTags("</font>")
+                .fragmentSize(2); //高亮数据展示多少个字符
+        sourceBuilder.highlighter(highlightBuilder);
+        request.source(sourceBuilder);
+        //4.执行查询
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //5.输出结果
+        System.out.println(response);
+    }
+
+    /**
+     * ardinality去重计数查询
+     */
+    @Test
+    void searchCardinality() throws IOException {
+        //1.创建request
+        SearchRequest request = new SearchRequest();
+        //2.创建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.aggregation(AggregationBuilders
+                .cardinality("agg")
+                .field("author"));
+        request.source(sourceBuilder);
+        //3.执行查询
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //4.输出结果
+        Cardinality agg = response.getAggregations().get("agg"); //agg为名称
+        System.out.println(agg.getValue());
+    }
+
+    /**
+     * .range范围统计，统计一定范围内出现的个数,时间范围、ip范围、数量范围
+     */
+    @Test
+    void searchRangeAgg() throws IOException {
+        //1.创建request
+        SearchRequest request = new SearchRequest();
+        //2.创建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.aggregation(AggregationBuilders
+                .range("agg")
+                .field("count")
+                .addUnboundedTo(0)
+                .addRange(0, 10));
+        request.source(sourceBuilder);
+        //3.执行查询
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //4.输出结果
+        System.out.println(response);
+    }
+
+    /**
+     * extended_stats统计聚合查询，查询指定field的最大值、最小值、平均值...
+     */
+    @Test
+    void searchExtendedStats() throws IOException {
+        //1.创建request
+        SearchRequest request = new SearchRequest();
+        //2.创建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.aggregation(AggregationBuilders.extendedStats("agg").field("count"));
+        request.source(sourceBuilder);
+        //3.执行查询
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //4.输出结果
+        ExtendedStats agge = response.getAggregations().get("agge");
+        System.out.println(agge.getMax()); //最大值
+    }
+
+    /**
+     * deo_polygon: 以多个点确认一个多边形
+     */
+    @Test
+    void searchGeoPolygon() throws IOException {
+        //1.创建request
+        SearchRequest request = new SearchRequest();
+        //2.创建查询条件
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        List<GeoPoint> points = new ArrayList<>();
+        points.add(new GeoPoint(39.908404, 116.433733)); //lon经度
+        sourceBuilder.query(QueryBuilders
+                .geoPolygonQuery("location", points));
+        //3.执行查询
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        //4.输出结果
+        for (SearchHit hit : response.getHits().getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
     }
 }
